df34250784ae9b60297a4636267758a8ebf86a7e
Kevin Wessa <kjwessa@gmail.com>
2024-04-07 14:11:44 -0400

    build: remove tailwind test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-slate-500 text-5xl mt-4 pl-10">Test</h1>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
