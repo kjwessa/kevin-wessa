32b32110f0a07ae84c66d80aa8a69dad4e8c2d8c
Kevin Wessa <kjwessa@gmail.com>
2024-04-09 17:12:56 -0400

    feat: install General Sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: { display: ['"Humane VF"'] },
+    fontFamily: {
+      display: ['"Humane VF"', "sans-serif"],
+      body: ['"GeneralSans Variable"', "sans-serif"],
+    },
   },
   plugins: [],
 };
