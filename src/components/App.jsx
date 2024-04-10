eb09206a69e2970a18634656d24a966f503792dc
Kevin Wessa <kjwessa@gmail.com>
2024-04-10 15:02:29 -0400

    feat(Main): add component with basic marketing copy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
-    <div className="bg-champagne-light text-prussian-medium h-screen">
+    <div className="bg-champagne-light text-prussian-medium flex min-h-screen flex-col">
       <Header />
+      <Main />
     </div>
   );
 }
