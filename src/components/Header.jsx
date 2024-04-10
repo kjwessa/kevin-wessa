f1e23a04c5d823ccb0ac072ef7e308a5e8c3824d
Kevin Wessa <kjwessa@gmail.com>
2024-04-10 15:41:04 -0400

    feat(Footer): add Footer component with dynamic copyright year

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-center py-3">
       <div>
-        <h1 className="font-display text-5xl font-bold">K/W</h1>
+        <h1 className="font-display text-5xl font-medium">K/W</h1>
       </div>
     </header>
   );
