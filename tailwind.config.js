b0bd58949a8025e89c57be6b25506b8be0442e1f
Kevin Wessa <kjwessa@gmail.com>
2024-04-10 12:25:47 -0400

    feat: add Header component with Tailwind styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      display: ['"Humane VF"', "sans-serif"],
-      body: ['"GeneralSans Variable"', "sans-serif"],
+    extend: {
+      fontFamily: {
+        display: ['"Humane VF"', "sans-serif"],
+        body: ['"GeneralSans Variable"', "sans-serif"],
+        cool: ['"NewTitle Variable"', "sans-serif"],
+      },
+      colors: {
+        champagne: {
+          light: "#f5e2ca",
+          medium: "#ebd5bb",
+          dark: "#ebd8c1",
+          text: "#685551",
+        },
+        prussian: {
+          light: "#345C7E",
+          medium: "#1C3144",
+          dark: "#162736",
+        },
+      },
     },
   },
   plugins: [],
