efe4552192c34dcb0684eb6b5b74ea7d9c3b1448
Kevin Wessa <kjwessa@gmail.com>
2024-04-11 19:25:39 -0400

    feat: add links to social media

---
+++ src/data/socialLinks.js
@@ -0,0 +1,14 @@
+const socialLinks = [
+  {
+    name: "GitHub",
+    url: "https://github.com/kjwessa",
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/kjwessa/",
+  },
+  { name: "Facebook", url: "https://www.facebook.com/kevin.wessa.7" },
+  { name: "Instagram", url: "https://www.instagram.com/kjwessa/" },
+];
+
+export default socialLinks;
