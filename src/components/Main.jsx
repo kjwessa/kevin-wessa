1b74426163856bf3f9a86baf8bd80472107fcc1f
Kevin Wessa <kjwessa@gmail.com>
2024-04-12 09:42:17 -0400

    feat: icons inherit currentColor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
+import SocialIcons from "./SocialIcons";
+
 export default function Main() {
   return (
-    <section className="text-prussian-medium flex min-h-32 flex-grow flex-col items-center justify-center">
+    <section className="flex min-h-32 flex-grow flex-col items-center justify-center text-prussian-medium">
       <h2 className="font-display text-9xl leading-none">Kevin J. Wessa</h2>
-      <p className="font-body text-m -mt-4 leading-none">
+      <p className="text-m -mt-4 font-body leading-none">
         Designer / Developer / Photographer
       </p>
+      <SocialIcons />
     </section>
   );
 }
