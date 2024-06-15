c798f0d03b26ceeddfd2aa988604b3cbdb3a8427
Kevin Wessa <kjwessa@gmail.com>
2024-06-15 15:59:04 -0400

    refactor(perf): switch fonts to load with next local font

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,31 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const HumaneVF = localFont({
+  variable: "--font-humane-vf",
+  display: "swap",
+  src: [
+    {
+      path: "../fonts/Humane-VF.ttf",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
+});
+
+const GeneralSansVariable = localFont({
+  variable: "--font-general-sans-variable",
+  display: "swap",
+  src: [
+    {
+      path: "../fonts/GeneralSans-Variable.ttf",
+      weight: "100 900",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Kevin Wessa - Designer, Developer, Photographer",
@@ -14,9 +39,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${HumaneVF.variable} ${GeneralSansVariable.variable}`}
+    >
       <body>
-        {children} <Analytics />
+        {children}
+        <Analytics />
       </body>
     </html>
   );
