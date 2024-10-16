5a1bcde4dd42ce999a7c041230f90a3eb62c50e2
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2024-10-15 22:25:42 -0400

    feat(ui): add home page hero and home page CTA elements

---
+++ src/app/(frontend)/home/page.tsx
@@ -0,0 +1,193 @@
+export default function Home() {
+  return(<>
+    <section className="relative bg-orange-100 overflow-hidden p-8 min-h-screen">
+      <div className="flex justify-between items-center">
+        <div className="w-1/4 relative">
+          <img
+            className="rounded-xl object-cover w-full h-full"
+            src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f01df7eead46798f360ed6_Person%20003.webp"
+            alt="Person 003"
+          />
+          <div className="absolute inset-0 bg-red-900/20 rounded-xl" />
+        </div>
+
+        <div className="text-center">
+          <h1 className="text-8xl font-bold uppercase leading-tight">
+            <br />Bold Founders
+          </h1>
+        </div>
+
+        <div className="w-1/4 relative">
+          <img
+            className="rounded-xl object-cover w-full h-full"
+            src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f01df7eead46a70e360edd_Avatar%20001%20(comp).webp"
+            alt="Avatar 001"
+          />
+          <div className="absolute inset-0 bg-red-900/20 rounded-xl" />
+        </div>
+      </div>
+
+    
+      <div className="absolute top-1/4 left-1/4 -translate-x-1/2 w-1/6 aspect-[3/2] transform rotate-3">
+        <img
+          className="rounded-xl object-cover w-full h-full"
+          src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f01df7eead4606ac360ee1_Avatar%20003%20(comp).webp"
+          alt="Avatar 003"
+        />
+        <div className="absolute inset-0 bg-red-900/20 rounded-xl" />
+      </div>
+
+      <div className="absolute bottom-1/4 right-1/4 translate-x-1/2 w-1/6 aspect-[3/2] transform -rotate-2">
+        <img
+          className="rounded-xl object-cover w-full h-full"
+          src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f01df7eead46798f360ed6_Person%20003.webp"
+          alt="Person 003"
+        />
+        <div className="absolute inset-0 bg-red-900/20 rounded-xl" />
+      </div>
+
+      <div className="absolute top-1/2 left-3/4 -translate-y-1/2 w-1/6 aspect-[3/2] transform rotate-1">
+        <img
+          className="rounded-xl object-cover w-full h-full"
+          src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f01df7eead46a70e360edd_Avatar%20001%20(comp).webp"
+          alt="Avatar 001"
+        />
+        <div className="absolute inset-0 bg-red-900/20 rounded-xl" />
+      </div>
+    </section>
+    <section className="text-orange-100 border-b-orange-100/[0.5] border-b-2 py-8 relative w-full border-solid bg-red-600">
+      <div className="items-center flex-col justify-center relative flex max-w-xs m-auto">
+        <div className="mt-0 mx-0 mb-5 text-xl" id="div-1">
+          <p className="text-center">The beginning of something great. </p>
+        </div>
+        <a className="items-center self-center justify-center flex w-16 min-h-[2.10rem] max-w-full border-2 border-orange-100 border-solid rounded-full" href="#down">
+          <div className="items-center cursor-pointer flex-col justify-center pr-1 relative flex overflow-hidden">
+            <div className="items-center justify-center flex p-1">
+              <img className="align-middle inline-block w-3 h-3 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f3e5d772cc9d7e2b7a340d_Arrow%20down%20thick%20light.svg" alt="Arrow down" />
+            </div>
+            <div className="items-center justify-center absolute top-[-100%] flex p-1">
+              <img className="align-middle inline-block w-3 h-3 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f3e5d772cc9d7e2b7a340d_Arrow%20down%20thick%20light.svg" alt="Arrow down" />
+            </div>
+          </div>
+        </a>
+      </div>
+    </section>
+    
+<section className="text-orange-100 relative bg-orange-100">
+  <div
+    className='bg-[url("https://assets.website-files.com/63f01df7eead46b8df360d90/63f043ea04f1d7256a19cc3a_Light%20noise.webp")] bottom-0 left-0 opacity-[0.06] absolute top-0'
+    style={{
+      backgroundSize: "240px 240px",
+    }}
+  />
+  <div className="px-10 w-full py-0 col-span-1 row-span-1" id="div-1">
+    <div className="w-full max-w-[105.00rem] m-auto">
+      <div className="bg-red-600 rounded-bl-xl rounded-br-xl auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[1fr] min-h-[35.00rem]">
+        <div className="px-20 w-full py-0" id="div-2">
+          <div className="auto-cols-fr grid-cols-[1fr_1fr] grid-rows-[auto] py-20 grid" id="div-3">
+            <div className="content-between self-stretch auto-cols-fr grid-cols-[1fr] grid-rows-[auto_1fr] py-10 pr-10 relative grid h-full gap-[3.88rem] col-span-1 row-span-1" id="div-4">
+              <div className="col-span-1 row-span-1 text-[9.00rem] leading-none uppercase">
+                <h2>
+                  The moment is now
+                </h2>
+              </div>
+              <div className="items-start self-end flex-col flex max-w-[19.00rem]">
+                <div className="mt-0 mx-0 mb-8 text-[1.38rem] leading-7" id="div-5">
+                  <p>We are dedicated to finding the most tenacious and ambitious minds in four key industries.</p>
+                </div>
+                <a className="items-center justify-start py-3 px-4 relative flex min-h-[2.10rem] max-w-full border-2 border-orange-100 border-solid rounded-full gap-6" href="https://capital-template.webflow.io/verticals">
+                  <div className="items-center cursor-pointer flex-col justify-center pr-1 relative flex overflow-hidden text-sm font-medium uppercase">
+                    <div>Slice Away</div>
+                    <div className="bottom-[-100%] absolute">
+                      <div>Slice Away</div>
+                    </div>
+                  </div>
+                  <div className="items-center cursor-pointer flex-col justify-center pr-1 relative flex overflow-hidden">
+                    <div className="items-center justify-center flex p-1">
+                      <img className="align-middle inline-block w-2 h-2 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f3e4e32859c8983993cad7_Arrow%20upright%20light.svg" />
+                    </div>
+                    <div className="items-center bottom-[-100%] justify-center left-[-100%] absolute flex p-1">
+                      <img className="align-middle inline-block w-2 h-2 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f3e4e32859c8983993cad7_Arrow%20upright%20light.svg" />
+                    </div>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div className="auto-cols-fr grid-cols-[1fr_1fr_1fr] grid-rows-[auto_auto_auto] grid gap-[0.13rem] col-span-1 row-span-1">
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-8 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f15371ac84aa2368c0dc96_AirBnB.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-10 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f4252f1dc162848f6aa8bf_Pendo%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-10 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f4253a596f1a867efa7e7d_Netflix%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-10 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f4254cb3d84fb31b9fe17c_Slack%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-16 h-8 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f42558a6a1ca78e12a584f_Stripe%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-11 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f42565369f9e272ff704a2_Upwork%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-20 h-8 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f42574b054e94158087ddf_Gusto%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-28 h-5 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f4257d2a45bb23c2b234ad_Attentive%20light.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="bg-red-500 items-center justify-center flex rounded-xl col-span-1 row-span-1">
+                <div className="items-center justify-center pt-[90%] relative flex w-full h-full">
+                  <div className="items-center bottom-0 justify-center left-0 absolute top-0 flex p-8">
+                    <img className="align-middle inline-block w-24 h-11 max-w-full" src="https://cdn.prod.website-files.com/63f01df7eead46b8df360d90/63f42588b054e982aa0881f0_Dribbble%20light.svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+    </>   
+  );
+} 
+
+
