f0c86b24251b14e086883c77373fcdc448d4dffb
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-01-07 13:41:22 -0500

    chore(cms): initial database migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250107_182540_initial from './20250107_182540_initial';
+import * as migration_20250107_184058_initial from './20250107_184058_initial';
 
 export const migrations = [
   {
-    up: migration_20250107_182540_initial.up,
-    down: migration_20250107_182540_initial.down,
-    name: '20250107_182540_initial'
+    up: migration_20250107_184058_initial.up,
+    down: migration_20250107_184058_initial.down,
+    name: '20250107_184058_initial'
   },
 ];
