b1efd510998d2ef5e681fce21fae3fdd4b24f9a2
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-01-10 17:34:28 -0500

    feat(ui): add grain effect to inner layout

--- src/app/(frontend)/(inner)/layout.tsx
+++ src/app/(frontend)/(inner)/layout.tsx
@@ -1,6 +1,7 @@
 import { type PropsWithChildren } from 'react'
 import { type Metadata } from 'next'
 import { Footer } from '@/Footer/Component'
+import { Grain } from '@/components/Grain'
 
 export const metadata: Metadata = {
   title: {
@@ -12,8 +13,10 @@ export const metadata: Metadata = {
 export default function InnerLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <main className="flex-1">{children}</main>
-      <Footer />
+      <Grain>
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </Grain>
     </div>
   )
 }
