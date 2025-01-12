b915288cd9dc2f6a52deda1230cf0f1ec50b64e0
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-01-12 15:13:26 -0500

    feat(ui): add journal skeleton

---
+++ src/app/(frontend)/(inner)/journal/page.tsx
@@ -0,0 +1,37 @@
+import { getPayload } from 'payload'
+import configPromise from '@payload-config'
+import { SectionBeta } from '@/components/layout/SectionBeta'
+import { ContainerBeta } from '@/components/layout/ContainerBeta'
+
+import { Page } from '@/components/layout/Page'
+
+export const revalidate = 21600 // 6 hours
+
+export default async function BlogPage() {
+  try {
+    const payload = await getPayload({ config: configPromise })
+    const [posts, categories] = await Promise.all([
+      payload.find({
+        collection: 'posts',
+        limit: 100,
+        sort: '-publishedOn',
+        where: {
+          _status: {
+            equals: 'published',
+          },
+        },
+        depth: 1,
+      }),
+      payload.find({
+        collection: 'categories',
+        limit: 1000,
+        sort: '-publishedOn',
+      }),
+    ])
+
+    return <Page theme="dark">test</Page>
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
