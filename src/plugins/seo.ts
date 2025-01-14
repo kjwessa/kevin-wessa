a2efc4a1e7ed297d63488086da42cc9079efdb4d
Kevin Wessa <39201312+kjwessa@users.noreply.github.com>
2025-01-14 15:06:28 -0500

    feat(meta): update SEO plugin with dynamic values

--- src/plugins/seo.ts
+++ src/plugins/seo.ts
@@ -1,20 +1,23 @@
 import { seoPlugin } from '@payloadcms/plugin-seo'
 import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
-import { Page, Post } from '@/payload-types'
+import { Page } from '@/payload-types'
+import { siteConfig } from '@/config/site'
 
-const generateTitle: GenerateTitle<Page | Post> = ({ doc }: { doc: any }) => {
-  return doc?.title ? `${doc.title} | Brewww Studio` : 'Brewww Studio '
+const generateTitle: GenerateTitle<Page> = ({ doc }: { doc: any }) => {
+  return doc?.title
+    ? siteConfig.meta.seo.titleTemplate.replace('%s', doc.title)
+    : siteConfig.meta.seo.defaultTitle
 }
 
-const generateURL: GenerateURL<Page | Post> = ({ doc }: { doc: any }) => {
-  if (!doc?.slug) return process.env.NEXT_PUBLIC_SERVER_URL!
+const generateURL: GenerateURL<Page> = ({ doc }: { doc: any }) => {
+  if (!doc?.slug) return siteConfig.meta.brand.domain
 
   // Add the /blog prefix for posts
   if (doc.collection === 'posts') {
-    return `${process.env.NEXT_PUBLIC_SERVER_URL!}/journal/${doc.slug}`
+    return `${siteConfig.meta.brand.domain}/journal/${doc.slug}`
   }
 
-  return `${process.env.NEXT_PUBLIC_SERVER_URL!}/${doc.slug}`
+  return `${siteConfig.meta.brand.domain}/${doc.slug}`
 }
 
 export const seo = seoPlugin({
